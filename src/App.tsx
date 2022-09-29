fa8cb0ac6ca2aa0b49fdf2fe91a27c7cb97e68b6
emericgautier <emericgtr@gmail.com>
2022-09-29 21:09:21 +0200

    Update App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { useRef, useState } from "react";
+import Message from "./components/Message";
+import { MessagesInt } from "./model";
 
-const App = () => {
+const App: React.FC = () => {
+  const inputMessage = useRef<HTMLInputElement | null>(null);
+  const [messData, setMessData] = useState<MessagesInt[]>([]);
+
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log(inputMessage);
+
+    // Logique d'envoie de données dans messData
+    if (inputMessage) {
+      const mess: MessagesInt = {
+        id: Math.round(Math.random() * Date.now()),
+        message: inputMessage?.current?.value,
+        date: Date.now()
+      }
+      // Incrémenter le tableau
+      setMessData((prevData) => [...prevData, mess]);
+    }
+
+    (document.getElementById("inputMessage") as HTMLInputElement).value = "";
+  }
+
   return (
     <div>
-      <h1>REACT</h1>
+      <h2>Poster un message</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder='Entrez un message' id="inputMessage" ref={inputMessage} />
+        <input type="submit" value="Envoyer" />
+      </form>
+      <h2>Liste des messages</h2>
+      <div>{messData?.map((mess) => (
+        <Message mess={mess} messData={messData} setMessData={setMessData} key={mess.id}/>
+      ))}</div>
     </div>
   );
 };
